36608b550218c698fb59149a7ed153a23ae3360d
ericnam <ericnam860@gmail.com>
2018-06-08 15:48:10 -0500

    Update config file

--- config/config.js
+++ config/config.js
@@ -3,9 +3,11 @@ let config = {
         host: '127.0.0.1',
         port: '5000',
         desc: 'local'
-    }
+    },
+    stage: {},
+    prod: {}
 }
 
 module.exports = (env) => {
     return env == null ? config.local : config[env];
-}
+} 
