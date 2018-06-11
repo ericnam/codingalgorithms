2b5e820b48578075f7839db6c69e2a7e10d6e24a
Eric Nam <eric.nam@pwc.com>
2018-06-11 13:41:01 -0500

    Navbar, along with stylesheet structure

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+
+    render() {
+        return (
+            <div class="navbar">
+                <span class="header">Coding Algorithms /</span>
+            </div>
+        );
+    }
+}
