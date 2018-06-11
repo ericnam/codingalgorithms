50f2523f2f4ecd9ddcd658edb4ef7bcc4abf4a30
Eric Nam <ericnam860@gmail.com>
2018-06-11 17:05:02 -0500

    Initial sidenav checkin

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Navbar from './components/Navbar';
+import SideNavbar from './components/SideNavbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar/>
+        {/* <Navbar /> */}
+        <SideNavbar />
       </div>
     );
   }
---
+++ client/src/components/SideNavbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class SideNavbar extends Component {
+
+    render() {
+        return (
+            <div class="side-navbar">
+                <div class="header">
+                    <span class="title">Coding Algorithms /</span>
+                </div>
+
+                <nav>
+                    <a>Bubble sort</a>
+                    <a>Bubble sort</a>
+                    <a>Bubble sort</a>
+                    <a>Bubble sort</a>
+                </nav>
+            </div>
+        );
+    }
+}
